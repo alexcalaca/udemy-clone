adee6a85ff658e67cd416cabd8bbbd2fd7c9d3ed
alexcalaca <alexcalaca@gmail.com>
2022-08-22 12:41:42 -0300

    test: add dummy data to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Course.delete_all
 
 30.times do
   course = Course.create!(title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote)    
+    description: Faker::TvShows::GameOfThrones.quote)
     puts "Title: #{course.title}. Description: #{course.description.first(10)}..."
-    puts "###"  
+    puts "###"
 end

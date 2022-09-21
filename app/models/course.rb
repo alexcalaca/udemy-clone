ee5def1387b40904dd185627ffd99ab8fc69f716
alexcalaca <alexcalaca@gmail.com>
2022-09-20 23:41:24 -0300

    feat: set the activity's owner to current_user by default

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord  
   include PublicActivity::Model
-  tracked
-  
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
   validates :title, :short_description,
             :language, :price,
             :level, presence: true

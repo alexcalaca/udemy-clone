12e950d42c2a69afb5d7b58132829f450d8532b0
alexcalaca <alexcalaca@gmail.com>
2022-09-20 23:52:29 -0300

    feat(routes): create route for home controller and activity action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
 
   get 'home/index'
+  get 'home/activity'
   get "privacy_policy", to: 'static_pages#privacy_policy'    
   root "static_pages#landing_page"
 end

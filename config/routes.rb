913b1c3bcc730b8c4dd1c3d894c08726363d2432
alexcalaca <alexcalaca@gmail.com>
2022-09-21 15:51:21 -0300

    feat: remove unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   resources :users, only: [:index]
-
   get 'home/index'
+
   get 'home/activity'
   get "privacy_policy", to: 'static_pages#privacy_policy'    
-  root "static_pages#landing_page"
+  
 end

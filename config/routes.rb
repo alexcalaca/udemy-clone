606b53022c2e6a21a76670902c7bf8f135d5facf
alexcalaca <alexcalaca@gmail.com>
2022-09-21 15:52:10 -0300

    feat: add new root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   resources :users, only: [:index]
-  get 'home/index'
 
   get 'home/activity'
-  get "privacy_policy", to: 'static_pages#privacy_policy'    
-  
+  get "privacy_policy", to: 'static_pages#privacy_policy'
+  get 'home/index'  
 end

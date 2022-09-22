38b4c3241395421bc9847542c45a2307a9da0a82
alexcalaca <alexcalaca@gmail.com>
2022-09-22 00:34:17 -0300

    feat(users_controller): implement edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update]
 	skip_before_action :authenticate_user!, :only => [:index]
 
 	def index
 		@q = User.ransack(params[:q])
 		@users = @q.result(distinct: true)
+	end
+
+	def edit				
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to users_path, notice: 'User roles were successfully updated'
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit({ role_ids: [] })
 	end
 end

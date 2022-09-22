09718200d7c9b02afabe29301b253ad1bd79e758
alexcalaca <alexcalaca@gmail.com>
2022-09-22 00:36:44 -0300

    feat(routes): set routes for edit, show and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do  
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
 
   get 'home/activity'
-  get "privacy_policy", to: 'static_pages#privacy_policy'
-  get 'home/index'  
+  get "privacy_policy", to: 'static_pages#privacy_policy'  
 end

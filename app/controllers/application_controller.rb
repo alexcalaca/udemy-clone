31c133a4dd756dbe35b4952713852669b6874286
alexcalaca <alexcalaca@gmail.com>
2022-09-23 23:41:26 -0300

    feat: customize the user_not_authorized method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   
   include Pundit
   protect_from_forgery
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :authenticate_user!
@@ -12,5 +11,12 @@ class ApplicationController < ActionController::Base
   
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Alert - You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
   end
 end

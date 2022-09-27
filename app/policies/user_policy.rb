d312e1f47b70f3c3624ecd7a8915e57bca0489c5
alexcalaca <alexcalaca@gmail.com>
2022-09-27 15:16:40 -0300

    feat: allow access to all to all records

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,7 @@
 class UserPolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  class Scope < Scope    
+    def resolve
+      scope.all
+    end
   end
 end

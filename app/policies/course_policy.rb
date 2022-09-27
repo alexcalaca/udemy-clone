e32b3e6625328211b69b59be9e9667a0a02f0de2
alexcalaca <alexcalaca@gmail.com>
2022-09-27 14:50:48 -0300

    feat: add authorization rules to course

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,8 +1,28 @@
 class CoursePolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  
+  class Scope < Scope    
+    def resolve
+      scope.all
+    end
+  end
+
+  def edit?    
+    @user.has_role?(:admin) || @record.user == @user
+  end
+
+  def update?
+    @user.has_role?(:admin) || record.user == user
+  end
+
+  def new?
+    @user.has_role? :teacher
+  end
+
+  def create?
+    @user.has_role? :teacher
+  end
+
+  def destroy?
+    @user.has_role?(:admin) || record.user_id == user.id
   end
 end

82d6f741b2226565904449b66eee566a88c8745d
alexcalaca <alexcalaca@gmail.com>
2022-09-30 23:44:53 -0300

    chore: perform unimportant changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :set_global_variables, if: :user_signed_in?
+  
   include Pundit
   protect_from_forgery
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  include PublicActivity::StoreController  
-
-  before_action :authenticate_user!
-
-  before_action :set_global_variables, if: :user_signed_in?
+  include PublicActivity::StoreController
+  
   
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)

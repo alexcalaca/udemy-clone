7719e6a5d10fb4f77fe8db1b1e11a00d1b3d54e0
alexcalaca <alexcalaca@gmail.com>
2022-10-04 00:46:37 -0300

    fix: solve redirect page devise issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery
+
   before_action :set_global_variables, if: :user_signed_in?
   
   include Pundit
-  protect_from_forgery
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  include PublicActivity::StoreController
-  
+  include PublicActivity::StoreController  
   
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
   end
-
+  
   private
 
   def user_not_authorized
-    flash[:alert] = "Alert - You are not authorized to perform this action."
+    flash[:alert] = "Alert - You are not authorized to perform this action."    
     redirect_back(fallback_location: root_path)
   end
 end
